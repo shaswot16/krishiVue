3cc0eac7abedf58041c16fc27d9d945262b43003
shaswot16 <shaswotpaudel58@gmail.com>
2023-11-03 22:34:56 +0545

    Image detection and Crop Recommendation api complete

--- api/main.py
+++ api/main.py
@@ -9,9 +9,12 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import requests
 from fastapi.middleware.cors import CORSMiddleware
+import joblib
+from pydantic import BaseModel
+import json
 
 MODEL = tf.keras.models.load_model("../Trained Model/1")
-
+CropPredictionModel = joblib.load("../Crop Prediction Model/CropPredictionModel.joblib")
 
 
 CLASS_NAMES = ['Potato___Early_blight',
@@ -26,6 +29,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class UserInput(BaseModel):
+    user_input: float
+
+class Package(BaseModel):
+    n: float
+    p: float
+    k: float
+    temperature:float	
+    humidity:float	
+    ph:float	
+    rainfall:float
+
+cropNameDict = {
+    0: 'apple',
+    1: 'banana',
+    2: 'blackgram',
+    3: 'chickpea',
+    4: 'coconut',
+    5: 'coffee',
+    6: 'cotton',
+    7: 'grapes',
+    8: 'jute',
+    9: 'kidneybeans',
+    10: 'lentil',
+    11: 'maize',
+    12: 'mango',
+    13: 'mothbeans',
+    14: 'mungbean',
+    15: 'muskmelon',
+    16: 'orange',
+    17: 'papaya',
+    18: 'pigeonpeas',
+    19: 'pomegranate',
+    20: 'rice',
+    21: 'watermelon'
+}
+
+    
 
 @app.get("/hello")
 async def root():
@@ -36,8 +77,16 @@ def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
+@app.post("/predictCrop")
+async def predictCrop(package:Package):
+    values = [package.n, package.p, package.k, package.temperature, package.humidity, package.ph,package.rainfall]
+    reshaped_values = [values]
+    prediction = CropPredictionModel.predict(reshaped_values)
+    
+    return {"prediction": cropNameDict[float(prediction)]}
 
-@app.post("/predict")
+
+@app.post("/predictCropDisease")
 async def predict(
     file: UploadFile = File(...)
 ):
